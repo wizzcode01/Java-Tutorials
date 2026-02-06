feb776f983b7f2a27a2673e34deaa9139048c3b8
Wisdom Dickson <dicksonwisdom005@gmail.com>
2026-02-06 23:41:03 +0100

    built a rock game with java

--- MyToDoApp/App.java
+++ MyToDoApp/App.java
@@ -1,6 +1,7 @@
 package MyToDoApp;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
@@ -8,23 +9,32 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("-----WELCOME TO WISDOM TIME SECHUDLE PROGRAM-----");
 
-        String[] tasks = null;
-        int[] time;
-        int size;
-
         System.out.print("How many schedule do you have to do: ");
-        size = scanner.nextInt();
+        int size = scanner.nextInt();
+
+        String[] tasks = new String[size];
+        int[] time = new int[size];
+        int totalTime = 0;
+
+        scanner.nextLine();
 
         for (int i = 0; i < size; i++) {
             System.out.print("What task do you have today: ");
             tasks[i] = scanner.nextLine();
-            scanner.nextLine();
+            // scanner.nextLine();
             System.out.print("Allocate time for it: ");
             time[i] = scanner.nextInt();
+            scanner.nextLine();
         }
 
-        System.out.println("\nYou have " + tasks);
-        System.out.printf("Total time to accomplish your schedule is: " + time + "hours");
+        System.out.println("\n======== YOUR SCHEDULE ========");
+        for (int i = 0; i < size; i++) {
+            System.out.println((i + 1) + ". " + tasks[i] + " -> " + time[i] + "hours");
+            totalTime += time[i];
+        }
+
+        // System.out.println("\nYou have " + tasks);
+        System.out.printf("Total time to accomplish your schedule is: " + totalTime + " hours");
 
         scanner.close();
     }
--- RockGame/App.java
+++ RockGame/App.java
@@ -1,8 +1,58 @@
 package RockGame;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        // ROCK, PAPER, SCISSORS
+        // GET CHOICE FROM THE USER
+        // GET RANDOM CHOICE FOR THE COMPUTER
+        // CHECK WIN CONDITIONS
+        // ASK TO PLAY AGAIN?
+        // GOODBYE MESSAGE
 
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String computerChoice;
+        String playerChoice;
+        String playAgain = "yes";
+        // = new String[];
+        String[] choice = { "rock", "paper", "scissors" };
+        do {
+            System.out.print("Enter your move(rock, paper, scissors): ");
+            playerChoice = scanner.nextLine().toLowerCase();
+
+            if (!playerChoice.equals("rock") && !playerChoice.equals("paper") && !playerChoice.equals("scissors")) {
+                System.out.println("Invalid player choice input");
+                continue;
+            }
+
+            computerChoice = choice[random.nextInt(3)];
+
+            System.out.println("Computer choice: " + computerChoice);
+
+            // check for win answer
+            if (playerChoice.equals(computerChoice)) {
+                System.out.println("It's a tie!");
+            } else if (playerChoice.equals("rock") && computerChoice.equals("scissors")) {
+                System.out.println("You Win!");
+            } else if (playerChoice.equals("paper") && computerChoice.equals("rock")) {
+                System.out.println("You Win!");
+            } else if (playerChoice.equals("scissors") && computerChoice.equals("paper")) {
+                System.out.println("You Win!");
+            } else {
+                System.out.println("You Loose!, Computer Wins!");
+            }
+
+            System.out.println("continue playing?(yes/no): ");
+            playAgain = scanner.nextLine().toLowerCase();
+
+        } while (playAgain.equals("yes"));
+
+        System.out.println("Thanks for playing");
+
+        scanner.close();
     }
-
 }
