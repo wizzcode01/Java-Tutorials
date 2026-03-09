a294a0b74735f4f0b05a35257202c2adf512349f
Wisdom Dickson <dicksonwisdom005@gmail.com>
2026-03-09 18:37:33 +0100

    finsh multitreading

--- Multithreading/App.java
+++ Multithreading/App.java
@@ -3,10 +3,20 @@ package Multithreading;
 public class App {
     public static void main(String[] args) {
         // MyRunnable myRunning = new MyRunnable();
-        Thread thread1 = new Thread(new MyRunnable());
-        Thread thread2 = new Thread(new MyRunnable());
+        Thread thread1 = new Thread(new MyRunnable("PING"));
+        Thread thread2 = new Thread(new MyRunnable("PONG"));
+
+        System.out.println("GAME START!");
         thread1.start();
         thread2.start();
 
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted");
+        }
+
+        System.out.println("GAME OVER!");
     }
 }
--- Multithreading/MyRunnable.java
+++ Multithreading/MyRunnable.java
@@ -1,12 +1,19 @@
 package Multithreading;
 
 public class MyRunnable implements Runnable {
+
+    private final String text;
+
+    MyRunnable(String text) {
+        this.text = text;
+    }
+
     @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
             try {
                 Thread.sleep(1000);
-                System.out.println(i);
+                System.out.println(text);
             } catch (InterruptedException e) {
                 System.out.println("Thread was interrupted");
             }
