86d6e9c32f1a1d9e0fc615d2ef894e67155ced6d
Wisdom Dickson <dicksonwisdom005@gmail.com>
2026-03-24 15:38:04 +0100

    finish alarm clock project

---
+++ AlarmClock/App.java
@@ -0,0 +1,33 @@
+package AlarmClock;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+import java.lang.Thread;
+
+public class App {
+    public static void main(String[] args) {
+        // JAVA ALARM CLOCK
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+        String filePath = "AlarmClock/file_example_WAV_1MG.wav";
+        while (alarmTime == null) {
+            try {
+                System.out.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
+
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid format, Please use our HH:MM:SS");
+            }
+        }
+
+        AlarmClock alarmClock = new AlarmClock(alarmTime, filePath, scanner);
+        Thread alarmThread = new Thread(alarmClock);
+        alarmThread.start();
+
+    }
+}
